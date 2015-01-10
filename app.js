39385293678f516fb541646d5420836d931cf98e
vish1562 <vishal1562@gmail.com>
2015-01-10 13:03:12 -0800

    Mini App

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.post('/update', function(req,res) {
   var body = req.body;
   debug('finish ee emit');
-
+  console.log('The request was' + req);
   console.log('The body was' + body);
 //  javascript:post('http://localhost:3000/update', body); 
   request ({
