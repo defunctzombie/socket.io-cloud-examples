16ab10b1d3679e90516b1860823cc9147839a6bb
nickguo <nick.n.guo@gmail.com>
2015-01-11 09:26:28 -0800

    changed 'message' to 'payload'

--- app.js
+++ app.js
@@ -8,37 +8,27 @@ var app = express();
 // Routing -> serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
-
-
 app.use(bodyParser.urlencoded());
 
 app.post('/update', function(req,res) {
   var body = req.body;
   var id = 'ns1';
   var type = 'new message';  
-  debug('finish ee emit');
-  console.log('The request was' + req);
-  console.log('The body was' + body);
-//  javascript:post('http://localhost:3000/update', body); 
-  request ({
-   uri : "http://localhost:3000/update",
-   method : "POST",
-   followRedirect : true,
-   timeout : 5000, 
-  // body : body,
-   json : {
-            "id": id,
-	    "type": type,
-	    "message": body.message
-	  }
-   }, function (error, response, body2) {
-   console.log(body2);
-   });
+  debug('The request was' + req);
+  debug('The body was' + body);
 
-  res.json({
-  });
+  request ({ uri : "http://localhost:3000/update",
+             method : "POST",
+             followRedirect : true,
+             timeout : 5000, 
+             json : { "id": id,
+	                  "type": type,
+	                  "payload": body.message }
+           }, function (error, response, body2) {
+                  console.log(body2);
+           });
 
-  
+  res.json({ });
 });
 
 
