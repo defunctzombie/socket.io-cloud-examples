3e418389e01d67c84e7382c404753d87a4530766
Roman Shtylman <shtylman@gmail.com>
2015-02-08 12:01:03 -0800

    update to work with new cloud API

--- examples1/app.js
+++ examples1/app.js
@@ -5,6 +5,7 @@ var debug = require('debug')('chat');
 var request= require ('request');
 
 var CLOUD_URL = process.env.CLOUD_URL;
+var SOCKET_URL = process.env.SOCKET_URL;
 
 var app = express();
 
@@ -19,35 +20,45 @@ app.use(bodyParser.urlencoded());
 app.get('/', function(req, res, next) {
   res.locals.app_name = process.env.APP_NAME;
   res.locals.CONFIG = JSON.stringify({
-    CLOUD_URL: process.env.CLOUD_URL
+    CLOUD_URL: SOCKET_URL
   });
 
   res.render('index', { layout: false });
 });
 
-app.post('/update', function(req,res) {
+app.post('/update', function(req, res, next) {
 
   var body = req.body;
   var id = '/messages';
   var type = 'new message';
-  debug('The request was' + req);
-  debug('The body was' + body);
+  debug('The body was %j', body);
 
   var opt = {
-    uri: CLOUD_URL + '/update',
-    method : 'POST',
-    timeout : 5000,
-    json : {
-      'id': id,
-      'app': 'app1',
+    uri: CLOUD_URL + '/api/apps/app1/events',
+    method: 'POST',
+    headers: {
+      'authorization': process.env.API_KEY
+    },
+    timeout: 5000,
+    json: {
+      'namespace': id,
       'type': type,
       'payload': body.message
     }
   };
 
   request(opt, function(err, api_res, api_body) {
-    debug(body);
-    res.json({ });
+    if (err) {
+      return next(err);
+    }
+
+    if (api_res.statusCode !== 200) {
+      var error = new Error('Unable to post update');
+      return next(error);
+    }
+
+    debug(api_body);
+    res.sendStatus(200);
   });
 });
 
